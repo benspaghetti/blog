2d6a63c35d7c74528f75bdf28fc850f981c179dc
benspaghetti <60951599+benspaghetti@users.noreply.github.com>
2026-01-02 21:24:53 -0500

    update, y3fall

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1765048494',resources: [
+    cacheName: 'chirpy-1767407054',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
