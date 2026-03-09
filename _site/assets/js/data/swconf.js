582f612014dc84aa0962367d82fc167e49a395de
benspaghetti <60951599+benspaghetti@users.noreply.github.com>
2026-03-09 14:31:22 +0800

    fixed

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1773037322',resources: [
+    cacheName: 'chirpy-1773037855',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
